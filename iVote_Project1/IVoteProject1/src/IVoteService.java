aa8e5c359a12bcac85720e22497736bfc485e108
Hetal <hssakaria@cpp.edu>
2015-08-09 11:23:59 -0700

    Added few Classes. Implemented Student,Candidate,Question,and StudentID classes.

--- iVote_Project1/IVoteProject1/src/IVoteService.java
+++ iVote_Project1/IVoteProject1/src/IVoteService.java
@@ -1,4 +1,26 @@
+import java.util.Hashtable;
+import java.util.Map;
 
 public class IVoteService {
+	
+	int totalStudentsSubmitted;
+	Map<String,String> iVote = new Hashtable<String, String>();
+	
+	
+	
+	private Map<String, String> getiVote() {
+		return iVote;
+	}
+	private void setiVote(Map<String, String> iVote) {
+		this.iVote = iVote;
+	}
+	private int getTotalStudentsSubmitted() {
+		return totalStudentsSubmitted;
+	}
+	private void setTotalStudentsSubmitted(int totalStudentsSubmitted) {
+		this.totalStudentsSubmitted = totalStudentsSubmitted;
+	}
+
+	
 
 }
